73e95307777223ed09629b8b6ac39d4707e9e2e4
Matthias Redies <matthias.redies@posteo.de>
2022-01-28 21:53:45 +0100

    add first bet

--- roulette.sol
+++ roulette.sol
@@ -3,19 +3,19 @@
 pragma solidity ^0.8.11;
 
 contract roulette{
-    struct bet{
+    struct Bet{
         address payable bettor;
         uint bet_blocknumber;
         uint[] winning_numbers;
-        uint[] winnig_amount;
+        uint winnig_amount;
     }
 
     address owner;
     uint security;
     bytes32 random_seed;
-    mapping(uint => bet) bet_list;
-    uint first = 1;
-    uint last = 0;
+    mapping(uint => Bet) bet_list;
+    uint first_bet = 1;
+    uint last_bet = 0;
 
     modifier shuffle {
         random_seed = keccak256(abi.encodePacked(random_seed,
@@ -23,7 +23,7 @@ contract roulette{
                                              block.timestamp,
                                              block.coinbase,
                                              gasleft()));
-        for(uint i = block.number - security; i <= block.number; i++){
+        for(uint i = max(0, block.number - security); i <= block.number; i++){
             random_seed = keccak256(abi.encodePacked(random_seed, blockhash(i)));
         }
         _;
@@ -42,18 +42,28 @@ contract roulette{
     constructor (uint _security){
         owner = msg.sender;
         security = _security;
+        require(security > 0, "Can't have 0 security");
     }
 
-    function push_bet(bet calldata incoming_bet) private {
-        last += 1;
-        bet_list[last] = incoming_bet;
+    function push_bet(Bet memory incoming_bet) private {
+        last_bet += 1;
+        bet_list[last_bet] = incoming_bet;
     }
 
     function pop_bet() private {
-        require(last >= first);
-        delete bet_list[first];
-        first += 1;
+        require(last_bet >= first_bet);
+        delete bet_list[first_bet];
+        first_bet += 1;
     }
+
+    function max(uint a, uint b)  pure private returns(uint){
+        if(a > b){
+            return a;
+        }else{
+            return b;
+        }
+    }
+
 
     function getrandom(uint nmax) private shuffle returns(uint) {
         uint x = uint(random_seed) % nmax;
@@ -61,10 +71,43 @@ contract roulette{
     }
 
     function payout_winnings() public shuffle{
+        uint i = first_bet;
+        while(i <= last_bet){
+            if(bet_list[i].bet_blocknumber > block.number - security){
+                break;
+            }else{
+                bool won = false;
+                uint ball = getrandom(37);
+                for(uint j = 0; j < bet_list[i].winning_numbers.length; j++){
+                    if(bet_list[i].winning_numbers[j] == ball){
+                        won = true;
+                    }
+                }
 
+                if(won){
+                    bet_list[i].bettor.transfer(bet_list[i].winnig_amount);
+                }
+                pop_bet();
+            }
+        }
+
+    }
+
+    function bet_odd(uint amount) public{
+        uint winning_amount = 2 * amount;
+        require(10 * winning_amount <= address(this).balance, "Stakes to high for me");
+
+        uint[] memory winning_numbers;
+        for(uint i = 0; i < 18; i++){
+            winning_numbers[i] =  (i*2) + 1;
+        }
+
+        push_bet(Bet(payable(msg.sender), block.number, winning_numbers, winning_amount));
     }
 
     function close_casion() public onlyOwner{
         selfdestruct(payable(owner));
     }
-}
+
+    function deposite() external payable onlyOwner{}
+} 
